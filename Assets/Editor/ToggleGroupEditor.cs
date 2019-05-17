6aabf79a83579551137b12a1d9826c597153a857
edingeldein <erich.dingeldein@und.edu>
2019-05-17 17:19:17 -0500

    Initial commit

---
+++ Assets/Editor/ToggleGroupEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ToggleGroupConfig))]
+public class ToggleGroupEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        var toggleGroupConfig = (ToggleGroupConfig)target;
+        if(GUILayout.Button("Draw Toggle Buttons"))
+        {
+            toggleGroupConfig.UpdateButtons();
+        }
+    }
+}
---
+++ Assets/Scripts/ToggleGroupConfig.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class ToggleGroupConfig : MonoBehaviour
+{
+    public int NumberOfButtons;
+    public GameObject TogglePrefab;
+
+    private List<GameObject> _toggleButtons;
+    private ToggleGroup _toggleGroup;
+    private GameObject _grouper;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _grouper = transform.Find("Grouper").gameObject;
+        _toggleGroup = _grouper.GetComponent<ToggleGroup>();
+
+        _toggleButtons = GetToggleButtons(_grouper);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void UpdateButtons()
+    {
+        DestroyButtons();
+        CreateButtons(NumberOfButtons);
+    }
+
+    private List<GameObject> GetToggleButtons(GameObject grouper)
+    {
+        var list = new List<GameObject>();
+        foreach(Transform trans in grouper.transform)
+        {
+            list.Add(trans.gameObject);
+        }
+        return list;
+    }
+
+    private void DestroyButtons()
+    {
+        foreach (var toggle in _toggleButtons)
+        {
+            DestroyImmediate(toggle);
+        }
+        _toggleButtons = new List<GameObject>();
+    }
+
+    private void CreateButtons(int num)
+    {
+        for(int i = 0; i < NumberOfButtons; i++)
+        {
+            var toggle = Instantiate(TogglePrefab, _toggleGroup.transform);
+            toggle.GetComponent<Toggle>().group = _toggleGroup.GetComponent<ToggleGroup>();
+
+            _toggleButtons.Add(toggle);
+        }
+    }
+
+}
